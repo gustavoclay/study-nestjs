b98850213fc4b1f9c298b5b815682206d78fd146
gustavoclay <gustavoclay@gmail.com>
2022-01-04 23:06:49 -0300

    add aula 2 todo puc minas

---
+++ puc/api_web_services/aula_todo/src/controllers/todoController.js
@@ -0,0 +1,20 @@
+
+exports.get = (req, res) => {
+    res.send('OK GET');
+}
+
+exports.getAll = (req, res) => {
+    res.send('OK GET ALL');
+}
+
+exports.add = (req, res) => {
+    res.send('OK ADD');
+}
+
+exports.update = (req, res) => {
+    res.send('OK UPDATE');
+}
+
+exports.delete = (req, res) => {
+    res.send('OK DELETE');
+}
